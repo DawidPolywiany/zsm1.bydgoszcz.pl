20f7a395315606baf8eb20957e124dc93b7cbdea
DawidPolywiany <Dawidpolyvjanyj@gmail.com>
2025-01-15 11:09:18 +0100

    Update-Head

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const route = express.Router();
 
-route.get('/', (req, res) => {
+route.get('/', async (req, res) => {
+    _home = req.db.collection('home');
+    _data = await _home.findOne({_name: "header"});
+    
     res.render('index', {
-        svg: req.svg
+        svg: req.svg,
+        data: _data
     });
 });
 
